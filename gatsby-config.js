9232dd3eda230da795bcab92bd06469637e27d61
Luca Barbetti <l.barbetti@topsolution.it>
2018-10-15 12:45:50 +0200

    Restore the manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Top Solution',
   },
   plugins: [
     {
@@ -11,18 +11,18 @@ module.exports = {
       },
     },
     'gatsby-plugin-react-helmet',
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'top-solution',
+        short_name: 'topsolution',
+        start_url: '/',
+        background_color: '#312783',
+        theme_color: '#312783',
+        display: 'minimal-ui',
+        icon: 'src/images/ts-icon/logo.png', // This path is relative to the root of the site.
+      },
+    },
     // 'gatsby-plugin-offline',
     'gatsby-plugin-sass',
     
