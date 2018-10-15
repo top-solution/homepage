0919dbde024a16baa5453f9f3d9940b2c6150a1b
Luca Barbetti <l.barbetti@topsolution.it>
2018-10-15 10:35:07 +0200

    Repair the borken about us page

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Layout from '../../components/Layout'
 import PageWrapper from '../../components/PageWrapper/PageWrapper'
 
-import logoText from '../../images/ts-logo-text.png'
+// import logoText from '../../images/ts-logo-text.png'
 import i3pLogo from '../../images/logos/i3p.png'
 
 import './AboutUs.scss'
-// import IndexTopic from '../../components/IndexTopic/IndexTopic';
 
+const AboutUsMD = ({ markdownRemark }) => {
+  return (
+    <div dangerouslySetInnerHTML={ { __html: markdownRemark.html } }>
+
+    </div>
+  )
+}
+
+AboutUsMD.propTypes = {
+  markdownRemark: PropTypes.shape({
+    html: PropTypes.string,
+  }),
+}
 
 const AboutUs = () => (
   <div className="page-about-us">
     <Layout>
       <PageWrapper>
-        <div className="ts-logo">
+        {/* <div className="ts-logo">
           <img src={ logoText } alt="" />
-        </div>
+        </div> */}
 
-        {/* <StaticQuery
+        <h2>Chi siamo</h2>
+
+        <StaticQuery
           query={ graphql`
             {
               markdownRemark(fields: {slug: {regex: "//pages/about-us//"}}) {
@@ -27,9 +42,8 @@ const AboutUs = () => (
               }
             }
           ` }
-          render={ IndexTopic }
+          render={ AboutUsMD }
         />
-         */}
         <div className="logos">
           <img src={ i3pLogo } alt="Incubatore Imprese Innovative Politecnico Torino" />
         </div>
