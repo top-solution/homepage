bdbbecb5c5155b0ea3f3b80f3fd024150bd67b20
Luca Barbetti <l.barbetti@topsolution.it>
2018-10-18 16:21:21 +0200

    Auto scrolling on hexagons card collapsible

--- src/pages/Index/HexagonCard/HexagonCard.jsx
+++ src/pages/Index/HexagonCard/HexagonCard.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import Button from '../../../components/Button/Button'
 
-import SVGHexagon from '../../../images/hexagons/hexagon';
+import SVGHexagon from '../../../images/hexagons/hexagon'
 import ServiceDetails from '../ServiceDetails/ServiceDetails'
 import ServicePreview from '../ServicePreview/ServicePreview'
 
@@ -15,10 +15,11 @@ class HexagonCard extends Component {
     super(props)
   
     this.state = {
-      collapsed: true
+      collapsed: true,
     }
 
-    this.collapsibleRef = React.createRef();
+    this.collapsibleRef = React.createRef()
+    this.collapsibleButtonRef = React.createRef()
 
     this.handleCollapseClick = this.handleCollapseClick.bind(this)
   }
@@ -28,11 +29,20 @@ class HexagonCard extends Component {
       this.setState({ 
         collapsed: false,
         collapsibleHeight: this.collapsibleRef.current.clientHeight + 32,
-      });
+      })
 
       setTimeout(() => {
-        this.setState({ collapsibleHeight: this.collapsibleRef.current.clientHeight + 32 });
-      }, 400);
+        this.setState({ collapsibleHeight: this.collapsibleRef.current.clientHeight + 32 })
+      }, 400)
+
+      setTimeout(() => {
+        // TODO: polyfill for safari
+        this.collapsibleButtonRef.current.scrollIntoView({
+          behavior: 'smooth',
+          block: 'end', 
+          inline: 'nearest',
+        })
+      }, 500)
     } else {
       this.setState({ 
         collapsed: true,
@@ -47,38 +57,43 @@ class HexagonCard extends Component {
     }
 
     return (
-      <div id={ 'page-' + this.props.data.excerpt.frontmatter.icon } className={`hexagon-card ${this.props.accent || ''}`}>
-        <SVGHexagon height={ null } width={144} className="hexagon">
+      <div id={ `page-${ this.props.data.excerpt.frontmatter.icon }` } className={ `hexagon-card ${ this.props.accent || '' }` }>
+        <SVGHexagon height={ null } width={ 144 } className="hexagon">
           <g className="hexagon-icon" transform="scale(0.32) translate(18, 28)">        
-            <use xlinkHref={ `#${ this.props.data.excerpt.frontmatter.icon }` }/>
+            <use xlinkHref={ `#${ this.props.data.excerpt.frontmatter.icon }` } />
           </g>
         </SVGHexagon>
         <div className="card-content">
-          <ServicePreview data={this.props.data.excerpt} />
-          <Button
-            purple
-            active={!this.state.collapsed}
-            onClick={ this.handleCollapseClick }>
-            {'Scopri i servizi'}
-          </Button>
-          <div 
-            className={`card-content-collapsible ${this.state.collapsed? 'collapsed' : ''}`}
-            style={{ maxHeight: this.state.collapsibleHeight }}
-          >
-            <div ref={ this.collapsibleRef }>
-              <ServiceDetails data={this.props.data.details} />
+          <ServicePreview data={ this.props.data.excerpt } />
+          <div className="collapsible">
+            <Button
+              purple
+              active={ !this.state.collapsed }
+              onClick={ this.handleCollapseClick }>
+              {'Scopri i servizi'}
+            </Button>
+            <div className={ 'scroller' }
+              ref={ this.collapsibleButtonRef }>
+            </div>
+            <div 
+              className={ `card-content-collapsible ${ this.state.collapsed? 'collapsed' : '' }` }
+              style={ { maxHeight: this.state.collapsibleHeight } }
+            >
+              <div ref={ this.collapsibleRef }>
+                <ServiceDetails data={ this.props.data.details } />
+              </div>
             </div>
           </div>
         </div>
         
       </div>
-    );
+    )
   }
 }
 
 HexagonCard.propTypes = {
   icon: PropTypes.string,
   text: PropTypes.string,
-};
+}
 
-export default HexagonCard;
+export default HexagonCard
