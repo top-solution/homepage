054782c4ce2f4eff02f1522bb5f1e641f5aec9f6
Luca Barbetti <l.barbetti@topsolution.it>
2018-10-22 12:42:29 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,5 +51,25 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-127823250-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Enables Google Optimize using your container Id
+        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'dktdhd3j2o1vs.cloudfront.net',
+      },
+    },
   ],
 }
