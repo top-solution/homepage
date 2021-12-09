7e699fc64b38a7963e2d7a08b0f45c7e79236454
Matteo Marsala <m.marsala@topsolution.it>
2021-12-09 17:42:20 +0100

    svelte picture

--- src/customers.svelte
+++ src/customers.svelte
@@ -1,7 +1,14 @@
 <svelte:options tag="svelte-customers" />
 
-<div class="flex-container">
+<script>
+  import sveltePicture from "../public/build/picture.js";
+</script>
+
+<div class="component-container">
   <div class="title">I NOSTRI CLIENTI</div>
+  <div class="flex-container">
+    <svelte-picture style="img" name="a" scr="a" />
+  </div>
 </div>
 
 <style>
@@ -13,6 +20,11 @@
     display: flex;
     justify-content: center;
     margin: 70px 0;
+    flex-wrap: wrap;
+  }
+
+  .img {
+    flex: 0 0 20%;
   }
 
   .title {
---
+++ src/picture.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag="svelte-picture" />
+
+<script>
+  export let style;
+  export let src;
+  export let alt;
+</script>
+
+<picture class={style}>
+  <source
+    srcSet={`../img/customers/${src}@2x.png 2x, ../img/customers/${src}.png 1x`}
+  />
+  <img loading="lazy" src={`../img/customers/${src}.png`} alt={alt} />
+</picture>
