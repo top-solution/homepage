7fcc6d73db04073e5079d944f083f4760b59e1c8
Matteo Marsala <m.marsala@topsolution.it>
2021-12-15 10:16:08 +0100

    development software component  first draft

---
+++ src/DevelopmentSoftware.svelte
@@ -0,0 +1,40 @@
+<svelte:options tag="ts-development-software" />
+
+<div class="component-container">
+  <div class="main-title">SVILUPPO SOFTWARE</div>
+  <div class="flex-container">
+    <ts-blob interactive="true" shape="rectangle" style="max-width: 300px;">
+      <div style="text-align: center;">
+        <img src="../img/devices.svg" alt="devices icon" width="50" height="35" />
+        <b>EXAGON</b>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+      </div>
+    </ts-blob>
+    <ts-blob interactive="true" shape="rectangle" style="max-width: 300px;">
+      <div style="text-align: center;">
+        <b>OCTAGON</b>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+      </div>
+    </ts-blob>
+    <ts-blob interactive="true" shape="rectangle" style="max-width: 300px;">
+      <div style="text-align: center;">
+        <b>RECTANGLE</b>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+      </div>
+    </ts-blob>
+  </div>
+  <button type="button">scopri</button>
+</div>
+
+<style>
+  @import "css/main.css";
+  @import "css/normalize.css";
+  @import "css/style.css";
+
+  .flex-container {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 24px;
+    color: #312783;
+  }
+</style>
