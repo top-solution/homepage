25404d818ffd3c3329c8b7295042e1f93f9dc247
Matteo Marsala <m.marsala@topsolution.it>
2021-12-15 10:16:08 +0100

    Button component, development software component upgrade

---
+++ src/Button.svelte
@@ -0,0 +1,41 @@
+<svelte:options tag="svelte-button" />
+
+<script>
+  export let bg = "transparent";
+  export let textColor = "#312783";
+  export let text = "Scopri";
+  export let href;
+  export let interactive = false;
+
+  //Manage Style and mouse enter //FIXME
+</script>
+
+<div class="button">
+  <a {href} style={`background-color: ${bg}; color: ${textColor}`}>
+    <span class="spacing"> {text} </span>
+    <img
+      on:mouseenter={interactive === "true"}
+      src="../img/icons/rightArrow.svg"
+      alt="arrow icon"
+      width="16"
+      height="16"
+    />
+  </a>
+</div>
+
+<style>
+  @import "css/main.css";
+  @import "css/normalize.css";
+  @import "css/style.css";
+
+  .button {
+    padding: 8px 22px;
+    border-radius: 4px;
+    text-transform: uppercase;
+    border: 1px solid rgba(49, 39, 131, 0.5);
+  }
+
+  .spacing {
+    margin-left: 12px;
+  }
+</style>
--- src/DevelopmentSoftware.svelte
+++ src/DevelopmentSoftware.svelte
@@ -1,10 +1,19 @@
 <svelte:options tag="ts-development-software" />
 
+<script>
+  import Button from "./Button.svelte";
+</script>
+
 <div class="component-container">
   <div class="main-title">SVILUPPO SOFTWARE</div>
   <div class="flex-container">
-    <ts-blob interactive="true" shape="rectangle" style="max-width: 300px;">
-      <div style="text-align: center;">
+    <ts-blob
+      interactive="true"
+      shape="rectangle"
+      style="max-width: 300px;"
+      fill="#E3F1FA"
+    >
+      <div class="center">
         <p>
           <img
             src="../img/icons/devices.svg"
@@ -13,24 +22,63 @@
             height="35"
           />
         </p>
-        <b>EXAGON</b>
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+        <b>APPLICAZIONI WEB E MOBILE</b>
+        <p>
+          Single page web applications, rest api, micro-services, hybrid apps
+        </p>
       </div>
     </ts-blob>
-    <ts-blob interactive="true" shape="rectangle" style="max-width: 300px;">
+    <ts-blob
+      interactive="true"
+      shape="rectangle"
+      style="max-width: 300px;"
+      fill="#E3F1FA"
+    >
       <div style="text-align: center;">
-        <b>OCTAGON</b>
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+        <p>
+          <img
+            src="../img/icons/optionPaper.svg"
+            alt="devices icon"
+            width="50"
+            height="35"
+          />
+        </p>
+        <b>CONSULENZA SVILUPPO SW</b>
+        <p>Kick-off progetti, sviluppo e manutenzione, disaster recovery</p>
       </div>
     </ts-blob>
-    <ts-blob interactive="true" shape="rectangle" style="max-width: 300px;">
+    <ts-blob
+      interactive="true"
+      shape="rectangle"
+      style="max-width: 300px;"
+      fill="#E3F1FA"
+    >
       <div style="text-align: center;">
-        <b>RECTANGLE</b>
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+        <p>
+          <img
+            src="../img/icons/cloud.svg"
+            alt="devices icon"
+            width="50"
+            height="35"
+          />
+        </p>
+        <b>CLOUD COMPUTING</b>
+        <p>
+          Utilizziamo servizi cloud per rendere i progetti sempre
+          raggiungibili!!!
+        </p>
       </div>
     </ts-blob>
   </div>
-  <button type="button">scopri</button>
+  <div class="button-container">
+    <Button
+      bg="transparent"
+      textColor="#312783"
+      text="SCOPRI"
+      href="randompage.html"
+      interactive="true"
+    />
+  </div>
 </div>
 
 <style>
@@ -43,5 +91,10 @@
     justify-content: space-between;
     margin-bottom: 24px;
     color: #312783;
+  }
+
+  .button-container {
+    display: flex;
+    justify-content: center;
   }
 </style>
