803f30d79d2ace20cc5826ac179d666ece9f6c4f
Matteo Marsala <m.marsala@topsolution.it>
2021-12-15 09:23:24 +0100

    remove comments

--- src/Customers.svelte
+++ src/Customers.svelte
@@ -6,55 +6,6 @@
 
 <div class="component-container">
   <h1>I NOSTRI CLIENTI</h1>
-  <!-- <div class="flex-container">
-    <Picture base="customers" src="sky" alt="Sky" />
-    <Picture base="customers" src="reply" alt="Reply" />
-    <Picture base="customers" src="collins" alt="Collins" />
-    <Picture base="customers" src="sanmarco" alt="San Marco" />
-    <Picture base="customers" src="whirpool" alt="Whirpool" />
-    <Picture base="customers" src="confindustria" alt="Confindustria" />
-  </div>
-
-  <div class="flex-container">
-    <Picture base="customers" src="sanPaolo" alt="Intesa SanPaolo" />
-    <Picture base="customers" src="vittorioAlfieri" alt="Vittorio Alfieri" />
-    <Picture base="customers" src="unicredit" alt="Unicredit" />
-    <Picture base="customers" src="alpitour" alt="Alpitour" />
-  </div>
-
-  <div class="flex-container">
-    <Picture base="customers" src="poltronaFrau" alt="Poltrona Frau" />
-    <Picture base="customers" src="qualityGroup" alt="Quality Group" />
-    <Picture base="customers" src="costaCrociere" alt="Costa Crociere" />
-    <Picture base="customers" src="carrefour" alt="Carrefour" />
-    <Picture base="customers" src="arduino" alt="Arduino" />
-    <Picture base="customers" src="generali" alt="Generali" />
-    <Picture base="customers" src="avio" alt="Avio" />
-  </div>
-  <div class="flex-container">
-    <Picture base="customers" src="apl" alt="APL" />
-    <Picture base="customers" src="mido" alt="Mido" />
-    <Picture base="customers" src="norbertoRosa" alt="Norberto Rosa" />
-    <Picture base="customers" src="euroSearch" alt="Euro Search" />
-    <Picture base="customers" src="stellantis" alt="Stellantis" />
-    <Picture base="customers" src="magna" alt="Magna" />
-  </div>
-  <div class="flex-container">
-    <Picture base="customers" src="hotelCube" alt="Hotel Cube" />
-    <Picture base="customers" src="helsinn" alt="Helsinn" />
-    <Picture base="customers" src="torino" alt="Torino" />
-    <Picture
-      base="customers"
-      src="unioneIndustriale"
-      alt="Unione Industriale"
-    />
-    <Picture
-      base="customers"
-      src="aidp"
-      alt="Associazione Italiana per la Direzione del Personale"
-    />
-  </div> -->
-
   <div class="flex-container">
     <Picture base="customers" src="sky" alt="Sky"/>
     <Picture base="customers" src="reply" alt="Reply" />
