325ed29c6481e63169d2f5ed7f901808d0e15b79
Luca Barbetti <lbarbetti@live.it>
2021-12-16 12:39:39 +0100

    Try fixing the weird blue buttons

--- src/Button.svelte
+++ src/Button.svelte
@@ -82,7 +82,9 @@
     border: 0;
 
     transition: background-color var(--ts-transition-timing-quick)
-      var(--ts-transition-function-default);
+        var(--ts-transition-function-default),
+      border-color var(--ts-transition-timing-quick)
+        var(--ts-transition-function-default);
   }
 
   .button--primary {
@@ -91,23 +93,26 @@
   }
 
   .button--primary:hover {
-    background-color: var(--ts-blue-color-dark);
+    background-color: var(--ts-blue-color-light);
   }
 
   .button--secondary {
     color: var(--ts-blue-color);
+    border: 1px solid transparent;
     background-color: var(--ts-azure-color);
   }
 
   .button--secondary:hover {
-    color: white;
-    background-color: var(--ts-azure-color-dark);
+    color: var(--ts-blue-color);
+    border-color: var(--ts-blue-color);
+    background-color: var(--ts-azure-color-light);
   }
 
   .button--primary,
   .button--secondary {
     box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 4px -1px,
-      rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px;
+      rgba(0, 0, 0, 0.14) 0px 4px 5px 0px,
+      rgba(240, 177, 177, 0.12) 0px 1px 10px 0px;
   }
 
   .button--outlined {
