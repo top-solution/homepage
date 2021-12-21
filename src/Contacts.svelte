2703f650533bda61bcf1ec21be8775b2f338d2ca
Luca Barbetti <lbarbetti@live.it>
2021-12-21 10:04:11 +0100

    Move the map to a separate component

--- src/Contacts.svelte
+++ src/Contacts.svelte
@@ -11,7 +11,6 @@
   let contactUsOpen = false;
   let contactUsElement = null;
   let snackbarElement = null;
-  let mapElement = null;
 
   const mapCenter = { lat: 45.0876111, lng: 7.6729824 };
   let map = null;
@@ -32,132 +31,6 @@
     contactUsOpen = false;
     snackbarElement.show();
   }
-
-  function initMap() {}
-
-  onMount(async () => {
-    var script = document.createElement("script");
-    script.src =
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAZo_LJrBCbL6GOjCwtR41qPpoi4jn4NwA&callback=initMap";
-    script.async = true;
-
-    window.initMap = function () {
-      map = new google.maps.Map(mapElement, {
-        center: mapCenter,
-        zoom: 16,
-        mapTypeId: "roadmap",
-        disableDefaultUI: true,
-        // zoomControl: true,
-        // fullscreenControl: true,
-        // gestureHandling: "none",
-        styles: [
-          {
-            featureType: "all",
-            elementType: "geometry",
-            stylers: [{ color: "#1b2742" }],
-          },
-          {
-            featureType: "all",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#aab1bb" }],
-          },
-          {
-            featureType: "all",
-            elementType: "labels.text.stroke",
-            stylers: [{ color: "#000000" }],
-          },
-          {
-            featureType: "landscape",
-            elementType: "geometry",
-            stylers: [{ color: "#1b2742" }],
-          },
-          {
-            featureType: "landscape.man_made",
-            elementType: "geometry",
-            stylers: [{ visibility: "off" }],
-          },
-          {
-            featureType: "landscape.man_made",
-            elementType: "geometry",
-            stylers: [{ visibility: "on" }],
-          },
-          {
-            featureType: "landscape.man_made.buildings",
-            elementType: "geometry",
-            stylers: [{ stroke: "#2a416f" }],
-          },
-          {
-            featureType: "landscape.natural",
-            elementType: "geometry",
-            stylers: [{ color: "#04364d" }],
-          },
-          {
-            featureType: "landscape.natural.terrain",
-            elementType: "geometry",
-            stylers: [{ color: "#1b2742" }],
-          },
-          {
-            featureType: "poi",
-            elementType: "labels",
-            stylers: [{ visibility: "off" }],
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [{ color: "04364d" }, { visibility: "on" }],
-          },
-          {
-            featureType: "poi.park",
-            elementType: "labels.text",
-            stylers: [{ visibility: "off" }],
-          },
-          {
-            featureType: "poi.park",
-            elementType: "labels.icon",
-            stylers: [{ visibility: "off" }],
-          },
-          {
-            featureType: "road",
-            elementType: "geometry",
-            stylers: [{ color: "#2a416f" }, { stroke: "#1c2c4a" }],
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#aab1bb" }],
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.stroke",
-            stylers: [{ color: "#000000" }],
-          },
-          {
-            featureType: "road.local",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#aab1bb" }],
-          },
-          {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [{ color: "#101621" }],
-          },
-          {
-            featureType: "water",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#456264" }],
-          },
-        ],
-      });
-
-      const marker = new google.maps.Marker({
-        position: { lat: 45.0876111, lng: 7.6729824 },
-        icon: "img/icons/pin-map.svg",
-        map: map,
-      });
-    };
-
-    document.head.appendChild(script);
-  });
 </script>
 
 <ts-layout class="page-contacts">
@@ -192,34 +65,7 @@
     bind:this={snackbarElement}
     labelText="Grazie per aver inviato la tua candidatura spontanea!"
   />
-  <div id="page-contacts__map-container">
-    <div bind:this={mapElement} id="page-contacts__map" />
-    <ts-button
-      id="page-contacts__map-center-button"
-      variant="secondary"
-      icon={true}
-      on:click={() => {
-        map.setCenter(mapCenter);
-        map.setZoom(16);
-      }}><img src="img/icons/center-map.svg" alt="center map" /></ts-button
-    >
-    <ts-button
-      id="page-contacts__map-zoom-in-button"
-      variant="secondary"
-      icon={true}
-      on:click={() => {
-        map.setZoom(map.zoom + 1);
-      }}><img src="img/icons/zoom-in-map.svg" alt="zoom in map" /></ts-button
-    >
-    <ts-button
-      id="page-contacts__map-zoom-out-button"
-      variant="secondary"
-      icon={true}
-      on:click={() => {
-        map.setZoom(map.zoom - 1);
-      }}><img src="img/icons/zoom-out-map.svg" alt="zoom out map" /></ts-button
-    >
-  </div>
+  <ts-office-map />
 </ts-layout>
 
 <style>
@@ -267,57 +113,10 @@
     display: block;
   }
 
-  #page-contacts__map-container,
-  #page-contacts__map {
-    height: 600px;
-    width: 100%;
-  }
-
-  #page-contacts__map-container {
+  ts-office-map {
+    display: block;
+    margin-top: var(--ts-spacing-8);
     margin-bottom: var(--ts-spacing-15);
-    position: relative;
-  }
-
-  #page-contacts__map-center-button {
-    position: absolute;
-    top: var(--ts-spacing-2);
-    right: var(--ts-spacing-2);
-    width: 42px;
-  }
-
-  #page-contacts__map-center-button {
-    position: absolute;
-    top: var(--ts-spacing-2);
-    right: var(--ts-spacing-2);
-    width: 42px;
-  }
-
-  #page-contacts__map-center-button {
-    position: absolute;
-    top: var(--ts-spacing-2);
-    right: var(--ts-spacing-2);
-    width: 42px;
-  }
-
-  #page-contacts__map-zoom-in-button {
-    position: absolute;
-    bottom: calc(var(--ts-spacing-4) + 42px);
-    right: var(--ts-spacing-2);
-    width: 42px;
-  }
-
-  #page-contacts__map-zoom-out-button {
-    position: absolute;
-    bottom: var(--ts-spacing-3);
-    right: var(--ts-spacing-2);
-    width: 42px;
-  }
-
-  #page-contacts__map-center-button img,
-  #page-contacts__map-zoom-in-button img,
-  #page-contacts__map-zoom-out-button img {
-    width: 24px;
-    height: 24px;
   }
 
   @media only screen and (max-width: 900px) {
