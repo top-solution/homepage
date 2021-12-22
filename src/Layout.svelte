30729096959ad4fbd2e7550de23f7c4b62770f8c
Luca Barbetti <lbarbetti@live.it>
2021-12-22 12:09:59 +0100

    Add Layout component

---
+++ src/Layout.svelte
@@ -0,0 +1,25 @@
+<svelte:options tag="ts-layout" />
+
+<div class="layout">
+  <slot name="external" />
+  <div class="layout__content">
+    <slot />
+  </div>
+</div>
+
+<style>
+  @import "css/main.css";
+  @import "css/normalize.css";
+  @import "css/style.css";
+
+  .layout {
+    max-width: var(--ts-tablet-max);
+    margin: 0 auto;
+    padding: 0 var(--ts-spacing-3);
+    position: relative;
+  }
+
+  .layout__content {
+    min-height: calc(100vh - 621px);
+  }
+</style>
