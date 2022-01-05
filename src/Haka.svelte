a18d3b62bfce55a1336682fc00d17361df7c321a
Luca Barbetti <lbarbetti@live.it>
2022-01-05 10:31:28 +0100

    Add Haka copy & video

--- src/Haka.svelte
+++ src/Haka.svelte
@@ -27,39 +27,15 @@
 
 <ts-layout class="page-haka">
   <div class="page-haka__copy">
-    <h1 class="title-2">haka</h1>
+    <h1 class="title-2">測 HAKA</h1>
     <p>
-      Top Solution nasce dall’associazione di una grande <b>expertise</b> nel
-      settore della <b>consulenza informatica</b> e della
-      <b>gestione delle risorse umane</b>. Il haka di informatici è affiancato
-      da <b>psicologi del lavoro</b> per la realizzazione delle
-      <b>piattaforme HR</b>
-      e per la progettazione e la realizzazione di tutti i progetti rivolti alle
-      <b>risorse umane</b>.
+      Le <b>Risorse Umane</b> sono il cardine delle organizzazioni: le idee
+      guidano il
+      <b>cambiamento</b>, le <b>persone</b> lo realizzano.
     </p>
-  </div>
-  <div class="people">
-    <div class="people__spacer" />
-    <ts-haka-person name="Paolo Andruetto" companyrole="CEO" />
-    <ts-haka-person name="Gabriele Destefanis" companyrole="CTO" />
-    <div class="people__spacer" />
-    <ts-haka-person name="Luca Osti" companyrole="Backend Developer" />
-    <ts-haka-person name="Gabriele Spada" companyrole="Frontend Developer" />
-    <ts-haka-person name="Luca Barbetti" companyrole="Frontend Developer" />
-    <div class="people__spacer" />
-    <ts-haka-person name="Elisa Solinas" companyrole="Backend Developer" />
-    <ts-haka-person name="Federico Valetti" companyrole="Analista Funzionale" />
-    <div class="people__spacer" />
-    <ts-haka-person name="Matteo Marsala" companyrole="Frontend Developer" />
-    <ts-haka-person name="Martina Bilancieri" companyrole="UX/UI Designer" />
-    <ts-haka-person src="img/question_mark.svg" name="Vuoi far parte del haka?">
-      <ts-button
-        id="people__contact-us-button"
-        variant="primary"
-        component="button"
-        on:click={handleClick}>Candidati</ts-button
-      >
-    </ts-haka-person>
+    <video class="page-haka__video" controls>
+      <source src="videos/haka.mp4" type="video/mp4" />
+    </video>
   </div>
 
   <div bind:this={contactUsElement} class="contact-us">
@@ -80,40 +56,19 @@
     color: var(--ts-blue-color);
   }
 
-  .people {
-    margin: var(--ts-spacing-15) 0;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
+  .page-haka__copy,
+  .page-haka__copy h1 {
+    text-align: center;
   }
 
-  .people__spacer {
-    flex: 0 1 16.6666%;
-    max-width: 16.6666%;
+  .page-haka__copy h1 {
+    margin-top: var(--ts-spacing-15);
   }
 
-  ts-haka-person {
-    margin: var(--ts-spacing-7) 0;
-    flex: 0 1 33.3333%;
-    max-width: 33.3333%;
-  }
-
-  #people__contact-us-button {
-    margin-top: var(--ts-spacing-2);
-  }
-
-  .page-haka__giga-blob {
-    display: block;
-    position: absolute;
-    width: 1400px;
-    overflow: visible;
-    top: -430px;
-    left: -700px;
-    z-index: -1;
-  }
-
-  .page-haka__giga-blob-mobile {
-    display: none;
+  .page-haka__video {
+    width: 100%;
+    height: auto;
+    margin: var(--ts-spacing-3) 0;
   }
 
   .contact-us {
@@ -123,34 +78,6 @@
   @media only screen and (max-width: 900px) {
     .page-haka__copy {
       max-width: 80%;
-    }
-
-    .people {
-      margin: var(--ts-spacing-3) 0;
-    }
-
-    .page-haka__giga-blob {
-      display: none;
-    }
-
-    .page-haka__giga-blob-mobile {
-      display: block;
-      position: absolute;
-      width: 1400px;
-      overflow: visible;
-      top: -130px;
-      left: -270px;
-      z-index: -1;
-    }
-
-    ts-haka-person {
-      margin: var(--ts-spacing-1) 2px;
-      flex: 0 1 calc(50% - 4px);
-      max-width: calc(50% - 4px);
-    }
-
-    .people__spacer {
-      display: none;
     }
   }
 </style>
