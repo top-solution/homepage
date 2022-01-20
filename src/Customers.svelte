ed40976297f1ea68d84f34acfe651c82906935e1
Matteo Marsala <m.marsala@topsolution.it>
2022-01-20 16:42:14 +0100

    fix picture and team pages position

--- src/Customers.svelte
+++ src/Customers.svelte
@@ -1,48 +1,44 @@
 <svelte:options tag="ts-customers" />
 
-<script>
-  import Picture from "./Picture.svelte";
-</script>
-
 <div class="component-container">
   <h1>I NOSTRI CLIENTI</h1>
   <div class="flex-container">
-    <Picture base="customers" src="sky" alt="Sky" />
-    <Picture base="customers" src="reply" alt="Reply" />
-    <Picture base="customers" src="collins" alt="Collins" />
-    <Picture base="customers" src="sanMarco" alt="San Marco" />
-    <Picture base="customers" src="whirpool" alt="Whirpool" />
-    <Picture base="customers" src="confindustria" alt="Confindustria" />
+    <ts-picture base="customers" src="sky" alt="Sky" />
+    <ts-picture base="customers" src="reply" alt="Reply" />
+    <ts-picture base="customers" src="collins" alt="Collins" />
+    <ts-picture base="customers" src="sanMarco" alt="San Marco" />
+    <ts-picture base="customers" src="whirpool" alt="Whirpool" />
+    <ts-picture base="customers" src="confindustria" alt="Confindustria" />
 
-    <Picture base="customers" src="sanPaolo" alt="Intesa SanPaolo" />
-    <Picture base="customers" src="vittorioAlfieri" alt="Vittorio Alfieri" />
-    <Picture base="customers" src="unicredit" alt="Unicredit" />
-    <Picture base="customers" src="alpitour" alt="Alpitour" />
+    <ts-picture base="customers" src="sanPaolo" alt="Intesa SanPaolo" />
+    <ts-picture base="customers" src="vittorioAlfieri" alt="Vittorio Alfieri" />
+    <ts-picture base="customers" src="unicredit" alt="Unicredit" />
+    <ts-picture base="customers" src="alpitour" alt="Alpitour" />
 
-    <Picture base="customers" src="poltronaFrau" alt="Poltrona Frau" />
-    <Picture base="customers" src="qualityGroup" alt="Quality Group" />
-    <Picture base="customers" src="costaCrociere" alt="Costa Crociere" />
-    <Picture base="customers" src="carrefour" alt="Carrefour" />
-    <Picture base="customers" src="arduino" alt="Arduino" />
-    <Picture base="customers" src="generali" alt="Generali" />
-    <Picture base="customers" src="avio" alt="Avio" extension="svg" />
+    <ts-picture base="customers" src="poltronaFrau" alt="Poltrona Frau" />
+    <ts-picture base="customers" src="qualityGroup" alt="Quality Group" />
+    <ts-picture base="customers" src="costaCrociere" alt="Costa Crociere" />
+    <ts-picture base="customers" src="carrefour" alt="Carrefour" />
+    <ts-picture base="customers" src="arduino" alt="Arduino" />
+    <ts-picture base="customers" src="generali" alt="Generali" />
+    <ts-picture base="customers" src="avio" alt="Avio" extension="svg" />
 
-    <Picture base="customers" src="apl" alt="APL" />
-    <Picture base="customers" src="mido" alt="Mido" extension="svg" />
-    <Picture base="customers" src="norbertoRosa" alt="Norberto Rosa" />
-    <Picture base="customers" src="euroSearch" alt="Euro Search" />
-    <Picture base="customers" src="stellantis" alt="Stellantis" />
-    <Picture base="customers" src="magna" alt="Magna" extension="svg" />
+    <ts-picture base="customers" src="apl" alt="APL" />
+    <ts-picture base="customers" src="mido" alt="Mido" extension="svg" />
+    <ts-picture base="customers" src="norbertoRosa" alt="Norberto Rosa" />
+    <ts-picture base="customers" src="euroSearch" alt="Euro Search" />
+    <ts-picture base="customers" src="stellantis" alt="Stellantis" />
+    <ts-picture base="customers" src="magna" alt="Magna" extension="svg" />
 
-    <Picture base="customers" src="hotelCube" alt="Hotel Cube" />
-    <Picture base="customers" src="helsinn" alt="Helsinn" />
-    <Picture base="customers" src="torino" alt="Torino" />
-    <Picture
+    <ts-picture base="customers" src="hotelCube" alt="Hotel Cube" />
+    <ts-picture base="customers" src="helsinn" alt="Helsinn" />
+    <ts-picture base="customers" src="torino" alt="Torino" />
+    <ts-picture
       base="customers"
       src="unioneIndustriale"
       alt="Unione Industriale"
     />
-    <Picture
+    <ts-picture
       base="customers"
       src="aidp"
       alt="Associazione Italiana per la Direzione del Personale"
