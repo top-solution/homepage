38044f66ec69dcf7175082331a0906a0ae03ef3b
Matteo Marsala <m.marsala@topsolution.it>
2022-02-04 16:11:15 +0100

    lorem ipsum in privacy policy

--- src/PrivacyPolicy.svelte
+++ src/PrivacyPolicy.svelte
@@ -5,7 +5,35 @@
 </script>
 
 <ts-layout class="privacy-policy">
-  <div class="privacy-policy__container">Privacy policy page</div>
+  <div class="privacy-policy__container">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  </div>
 </ts-layout>
 
 <style lang="scss" global>
@@ -22,6 +50,10 @@
       background-color: variables.$ts-azure-color-light;
       border: 1px solid variables.$ts-azure-color;
       border-radius: 4px;
+
+      p {
+        margin-bottom: variables.$ts-spacing-1;
+      }
     }
   }
 </style>
