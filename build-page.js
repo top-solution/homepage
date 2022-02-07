b9e4470a522c4a1b4597d51b8b5a1694142047dd
Luca Barbetti <lbarbetti@live.it>
2022-02-07 11:44:26 +0100

    Try embedding manrope

--- build-page.js
+++ build-page.js
@@ -13,12 +13,63 @@ Handlebars.registerPartial(
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
     <link rel="manifest" href="site.webmanifest" />
     <link rel="shortcut icon" type="image/svg" href="./img/logo.svg" />
+    <style>
+      /* manrope-300 - latin */
+      @font-face {
+        font-family: 'Manrope';
+        font-style: normal;
+        font-weight: 300;
+        src: url('./fonts/manrope-v8-latin-300.eot'); /* IE9 Compat Modes */
+        src: local(''),
+            url('./fonts/manrope-v8-latin-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+            url('./fonts/manrope-v8-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+            url('./fonts/manrope-v8-latin-300.woff') format('woff'), /* Modern Browsers */
+            url('./fonts/manrope-v8-latin-300.ttf') format('truetype'), /* Safari, Android, iOS */
+            url('./fonts/manrope-v8-latin-300.svg#Manrope') format('svg'); /* Legacy iOS */
+      }
 
-    <link
-      href="http://fonts.googleapis.com/css?family=Roboto:200,300,400,500,600,700,900|Work+Sans:100,200,300,400,500,600,700,800,900&display=swap"
-      rel="stylesheet"
-      type="text/css"
-    />
+      /* manrope-regular - latin */
+      @font-face {
+        font-family: 'Manrope';
+        font-style: normal;
+        font-weight: 400;
+        src: url('./fonts/manrope-v8-latin-regular.eot'); /* IE9 Compat Modes */
+        src: local(''),
+            url('./fonts/manrope-v8-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+            url('./fonts/manrope-v8-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+            url('./fonts/manrope-v8-latin-regular.woff') format('woff'), /* Modern Browsers */
+            url('./fonts/manrope-v8-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+            url('./fonts/manrope-v8-latin-regular.svg#Manrope') format('svg'); /* Legacy iOS */
+      }
+
+      /* manrope-500 - latin */
+      @font-face {
+        font-family: 'Manrope';
+        font-style: normal;
+        font-weight: 500;
+        src: url('./fonts/manrope-v8-latin-500.eot'); /* IE9 Compat Modes */
+        src: local(''),
+            url('./fonts/manrope-v8-latin-500.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+            url('./fonts/manrope-v8-latin-500.woff2') format('woff2'), /* Super Modern Browsers */
+            url('./fonts/manrope-v8-latin-500.woff') format('woff'), /* Modern Browsers */
+            url('./fonts/manrope-v8-latin-500.ttf') format('truetype'), /* Safari, Android, iOS */
+            url('./fonts/manrope-v8-latin-500.svg#Manrope') format('svg'); /* Legacy iOS */
+      }
+
+      /* manrope-700 - latin */
+      @font-face {
+        font-family: 'Manrope';
+        font-style: normal;
+        font-weight: 700;
+        src: url('./fonts/manrope-v8-latin-700.eot'); /* IE9 Compat Modes */
+        src: local(''),
+            url('./fonts/manrope-v8-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+            url('./fonts/manrope-v8-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
+            url('./fonts/manrope-v8-latin-700.woff') format('woff'), /* Modern Browsers */
+            url('./fonts/manrope-v8-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
+            url('./fonts/manrope-v8-latin-700.svg#Manrope') format('svg'); /* Legacy iOS */
+      }
+    </style>
 `)
 
 Handlebars.registerPartial(
