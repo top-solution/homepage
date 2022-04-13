56f2927759431596f76bf723d66f125fc77d73da
Matteo Marsala <m.marsala@topsolution.it>
2022-04-13 12:43:19 +0200

    remove sass and update button, banner, triple blob

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  public: "./public/",
-  buildOptions: {
-    site: "https://topsolution.it",
-  },
+  publicDir: "./public/",
+  site: "https://topsolution.it",
   vite: {
     ssr: {
       external: ["svgo"],
