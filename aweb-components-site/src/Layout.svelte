03528a340fb993df55e887012515053934759a93
Matteo Marsala <m.marsala@topsolution.it>
2022-06-20 16:09:16 +0200

    remove svelte code

--- aweb-components-site/src/Layout.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:options tag="ts-layout" />
-
-<div class="layout">
-  <slot name="external" />
-  <div class="layout__content">
-    <slot />
-  </div>
-</div>
--- aweb-components-site/src/contacts.js
+++
@@ -1,6 +0,0 @@
-export const contacts = {
-  phone: "+39 0112488280",
-  email: "info@topsolution.it",
-  registeredOffice: "Corso Regina Margherita, 254, 10144 Torino TO",
-  operationalOffice: "Via Livorno, 60 B2 Lab, 10144 Torino TO",
-}
