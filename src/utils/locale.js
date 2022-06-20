c53561833ed974ac1c6cf04ee53d4fb9dcebca13
Marco-03-05-04 <marco.pnz04@gmail.com>
2022-06-20 10:11:13 +0200

    traduzione

--- src/utils/locale.js
+++ src/utils/locale.js
@@ -1,11 +1,14 @@
 export default {
   en: {
-    homeBanner: "Get in touch with us. Discover the opportunities for your business offered by our solutions.",
-    company: "COMPANY"
+    homeBanner: "Get in touch with us Discover the opportunities for your business offered by our solutions",
+    company: "COMPANY",
+    hrDevelopment: "HR SOLUTIONS",
   },
   it: {
-    homeBanner: "Scopri le opportunità per il tuo business offerte dalle nostre soluzioni. Entra in contatto con noi!",
-    company: "AZIENDA"
+    homeBanner: "Scopri le opportunità per il tuo business offerte dalle nostre soluzioni Entra in contatto con noi!",
+    company: "AZIENDA",
+    
+    hrDevelopment: "SVILUPPO DELLE RISORSE UMANE",
+
   },
 }
-
